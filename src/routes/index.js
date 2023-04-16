448a9961e73281365d56c2b146cdcfb07af629f4
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-04-16 19:19:52 -0300

    remove chat and update back

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const usersRoutes = require("./users.routes");
 const sessionsRoutes = require("./sessions.routes");
 const postsRoutes = require("./posts.routes");
 const commentsRoutes = require("./comments.routes");
-const chatRoutes = require("./chat.routes");
 
 const routes = Router();
 
@@ -12,6 +11,5 @@ routes.use("/", usersRoutes);
 routes.use("/", sessionsRoutes);
 routes.use("/", postsRoutes);
 routes.use("/", commentsRoutes);
-routes.use("/", chatRoutes);
 
 module.exports = routes;
